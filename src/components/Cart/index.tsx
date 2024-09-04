4b19ff201ac18652c39fb7f824ef5d40244db3a5
Alex Solda <dev.alexsolda@gmail.com>
2024-09-04 18:58:39 -0300

    feat: cart dropdown menu structure

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -3,20 +3,33 @@ import { ReactElement } from "react";
 import Button from "../Button";
 import BagIcon from "../Icons/BagIcon";
 import { useProducts } from "@/stores/Products";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
 
 const Cart = ():ReactElement => {
 
     const {cart} = useProducts();
 
     return (
-       <div className="relative">
+        <Menu>
+        <MenuButton className='relative'>
             {cart.length > 0 && (
             <div className="flex items-center justify-center absolute bg-green-800 rounded-full w-[20px] h-[20px] right-0 bottom-0">
                 <p className="text-xs">{cart.length}</p>
             </div>
             )}
             <Button icon={<BagIcon />} onlyIcon />
-       </div>
+        </MenuButton>
+
+        <MenuItems
+          transition
+          anchor="bottom end"
+          className='mt-3 bg-white rounded-lg'
+        >
+          <MenuItem>
+            <p className="w-[200px]">opa</p>
+          </MenuItem>
+        </MenuItems>
+      </Menu>
     )
 };
 
